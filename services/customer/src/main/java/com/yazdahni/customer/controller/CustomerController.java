bf885f95c3259c104b902cf70bb1fdadbbb1dc6d
Abid Ali <abid38ali@gmail.com>
2025-02-27 00:42:56 -0700

    Implemented Payment MS

--- services/customer/src/main/java/com/yazdahni/customer/controller/CustomerController.java
+++ services/customer/src/main/java/com/yazdahni/customer/controller/CustomerController.java
@@ -5,7 +5,6 @@ import com.yazdahni.customer.dto.CustomerResponse;
 import com.yazdahni.customer.service.CustomerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
