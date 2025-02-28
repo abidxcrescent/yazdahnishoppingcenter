7c83fea37479ceaa6dced8f0a35586b2acbac857
Abid Ali <abid38ali@gmail.com>
2025-02-27 17:44:06 -0700

    Fixed docker compose for postgres

--- services/notification/src/main/java/com/yazdahni/notification/email/EmailService.java
+++ services/notification/src/main/java/com/yazdahni/notification/email/EmailService.java
@@ -1,5 +1,6 @@
 package com.yazdahni.notification.email;
 
+import com.yazdahni.notification.kafka.order.Product;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
@@ -8,10 +9,14 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -33,7 +38,67 @@ public class EmailService {
                 mimeMessage,
                 MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
                 StandardCharsets.UTF_8.name());
+
+        final String templateName = EmailTemplates.PAYMENT_CONFIRMATION.getTemplate();
+
+        Map<String, Object> variables = new HashMap<>();
+
+        variables.put("customerName", customerName);
+        variables.put("amount", amount);
+        variables.put("orderReference", orderReference);
+
+        Context context = new Context();
+        context.setVariables(variables);
+        mimeMessageHelper.setSubject(templateName);
+
+        try{
+            String htmlTemplate = templateEngine.process(templateName, context);
+            mimeMessageHelper.setText(htmlTemplate, true);
+            mimeMessageHelper.setTo(destinationEmail);
+            mailSender.send(mimeMessage);
+            log.info("INFO - Email sent to {} successfully with template {}", destinationEmail, templateName);
+
+        }catch (MessagingException e){
+            log.warn("WARNING email could not be sent to {}", destinationEmail);
+        }
+
     }
 
+    @Async
+    public void sendOrderConfirmationEmail(
+            String destinationEmail,
+            String customerName,
+            BigDecimal amount,
+            String orderReference,
+            List<Product> products
+    ) throws MessagingException {
 
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, UTF_8.name());
+        messageHelper.setFrom("contact@aliboucoding.com");
+
+        final String templateName = EmailTemplates.ORDER_CONFIRMATION.getTemplate();
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("customerName", customerName);
+        variables.put("totalAmount", amount);
+        variables.put("orderReference", orderReference);
+        variables.put("products", products);
+
+        Context context = new Context();
+        context.setVariables(variables);
+        messageHelper.setSubject(EmailTemplates.ORDER_CONFIRMATION.getSubject());
+
+        try {
+            String htmlTemplate = templateEngine.process(templateName, context);
+            messageHelper.setText(htmlTemplate, true);
+
+            messageHelper.setTo(destinationEmail);
+            mailSender.send(mimeMessage);
+            log.info(String.format("INFO - Email successfully sent to %s with template %s ", destinationEmail, templateName));
+        } catch (MessagingException e) {
+            log.warn("WARNING - Cannot send Email to {} ", destinationEmail);
+        }
+
+    }
 }
